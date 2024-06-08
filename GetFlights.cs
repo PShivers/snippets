426297e538660967124f94cf0d0b630753013069
Paul Shivers <48158453+PShivers@users.noreply.github.com>
2024-06-08 17:58:20 -0400

    Create GetFlights.cs

---
+++ GetFlights.cs
@@ -0,0 +1,103 @@
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+public class FlightData
+{
+    public string icao24;
+    public string callsign;
+    public string origin_country;
+    public double time_position;
+    public double last_contact;
+    public double longitude;
+    public double latitude;
+    public double baro_altitude;
+    public bool on_ground;
+    public double velocity;
+    public double heading;
+    public double vertical_rate;
+    public int sensors;
+    public double geo_altitude;
+    public string sqawk;
+    public bool spi;
+    public int position_source;
+    public string category;
+}
+
+public class FlightDataManager : MonoBehaviour
+{
+    private async void Start()
+    {
+        string url = "https://opensky-network.org/api/states/all";
+        string accessKey = "18d9ac328be6cdbe249ab5f8523e9de8";
+        Dictionary<string, string> parameters = new Dictionary<string, string>
+        {
+            { "access_key", accessKey }
+        };
+
+        List<FlightData> flightData = await GetData(url, parameters);
+        Debug.Log("Data received: " + flightData.Count + " flights");
+    }
+
+    private async Task<List<FlightData>> GetData(string url, Dictionary<string, string> parameters)
+    {
+        List<FlightData> flightData = new List<FlightData>();
+
+        using (HttpClient client = new HttpClient())
+        {
+            HttpResponseMessage response = await client.GetAsync(url + ToQueryString(parameters));
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonString = await response.Content.ReadAsStringAsync();
+                dynamic jsonData = JsonUtility.FromJson<dynamic>(jsonString);
+
+                foreach (var flight in jsonData.states)
+                {
+                    FlightData newFlightObject = new FlightData
+                    {
+                        icao24 = flight[0],
+                        callsign = flight[1],
+                        origin_country = flight[2],
+                        time_position = flight[3],
+                        last_contact = flight[4],
+                        longitude = flight[5],
+                        latitude = flight[6],
+                        baro_altitude = flight[7],
+                        on_ground = flight[8],
+                        velocity = flight[9],
+                        heading = flight[10],
+                        vertical_rate = flight[11],
+                        sensors = flight[12],
+                        geo_altitude = flight[13],
+                        sqawk = flight[14],
+                        spi = flight[15],
+                        position_source = flight[16],
+                        category = flight[17]
+                    };
+                    flightData.Add(newFlightObject);
+                }
+            }
+            else
+            {
+                Debug.LogError("Failed to fetch data: " + response.StatusCode);
+            }
+        }
+
+        return flightData;
+    }
+
+    private string ToQueryString(Dictionary<string, string> parameters)
+    {
+        if (parameters.Count == 0)
+            return "";
+
+        List<string> parts = new List<string>();
+        foreach (var kvp in parameters)
+        {
+            parts.Add(string.Format("{0}={1}", Uri.EscapeDataString(kvp.Key), Uri.EscapeDataString(kvp.Value)));
+        }
+        return "?" + string.Join("&", parts.ToArray());
+    }
+}
