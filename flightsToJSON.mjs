1577839a2c55bd0d67bf99be77ff09365a32b886
Paul Shivers <48158453+PShivers@users.noreply.github.com>
2024-06-08 17:55:47 -0400

    Update flightsToJSON.mjs
    now works off opensky api

--- flightsToJSON.mjs
+++ flightsToJSON.mjs
@@ -1,38 +1,49 @@
 import fs from "fs";
 import axios from "axios";
 
-const getData = async (params) => {
-  let flightData = [];
-  for (let i = 0; i < 1; i++) {
-    let offset = 0;
-    if (i > 0) {
-      offset = i * 100;
-    }
+const getData = async (url, params = {}) => {
+  axios
+    .get(url, { params })
+    .then((response) => {
+      let flightData = [];
+      const data = response.data;
 
-    axios
-      .get("http://api.aviationstack.com/v1/flights", { params })
-      .then((response) => {
-        const data = response.data;
-        let newFlightData = data.data;
-        flightData.push(...newFlightData);
-
-        return flightData;
-      })
-      .catch((error) => {
-        console.log(error);
+      data.states.forEach((flight) => {
+        let newFlightObject = {
+          icao24: flight[0],
+          callsign: flight[1],
+          origin_country: flight[2],
+          time_position: flight[3],
+          last_contact: flight[4],
+          longitude: flight[5],
+          latitude: flight[6],
+          baro_altitude: flight[7],
+          on_ground: flight[8],
+          velocity: flight[9],
+          heading: flight[10],
+          vertical_rate: flight[11],
+          sensors: flight[12],
+          geo_altitude: flight[13],
+          sqawk: flight[14],
+          spi: flight[15],
+          position_source: flight[16],
+          category: flight[17],
+        };
+        flightData.push(newFlightObject);
       });
-  }
+      console.log(flightData);
+      return flightData;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 };
 
 let params = {
   access_key: "18d9ac328be6cdbe249ab5f8523e9de8",
 };
+let url = "https://opensky-network.org/api/states/all";
 
-await getData(params).then((res) => {
-  console.log("res", res);
-  // fs.writeFile("flightData.json", res, function (err) {
-  //   if (err) {
-  //     console.log(err);
-  //   }
-  // });
-});
+const flightData = await getData(url).then((res) =>
+  console.log("from promise")
+);
